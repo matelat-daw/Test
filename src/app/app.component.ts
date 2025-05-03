4520995d47266aa1047b9418f2cde1f987f99e75
matelat-daw <cesarmatelat@gmail.com>
2025-05-03 12:13:39 +0100

    Update 3-5-2025 12:13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  // standalone: true,
-  // imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    standalone: false
 })
 export class AppComponent {
   title = 'Test';
