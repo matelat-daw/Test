943575eecdf9524de04c8d24c102b1584e6c7829
matelat-daw <cesarmatelat@gmail.com>
2025-05-03 12:08:55 +0100

    Update 3-5-2025 12:08

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CommonModule,
-    HttpClientModule,
-    ReactiveFormsModule
-  ],
-  providers: [
-    provideClientHydration(),
-    provideHttpClient({ useFetch: true })
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        CommonModule,
+        ReactiveFormsModule], providers: [
+        provideClientHydration(),
+        provideHttpClient({ useFetch: true }),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
